1cc4e4ce1508fcb0d5a769c4338cdf900261c5f1
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-02-20 00:55:41 +0100

    commit Getest met Routes

--- src/NavbarComp.jsx
+++ src/NavbarComp.jsx
@@ -1,31 +1,49 @@
+import React from "react"
 import "bootstrap/dist/css/bootstrap.css";
-import { Nav, Navbar, Container } from "react-bootstrap";
-import logo from "./logo.svg";
 import "./NavbarComp.css";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Home from "./components/Home"
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import  logo  from "./logo.svg";
+import  About  from "./components/About";
+import  Contact  from "./components/Contact";
+import  Home from "./components/Home"
 
 
 function NavbarComp() {
     return (
-        <>
-            <Navbar bg="dark" variant="dark" expand="lg">
-                <Container>
-                    <Navbar.Brand>
-                       Logo <img src={logo} width="40px" height="40px" />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="Home">Home</Nav.Link>
-                            <Nav.Link href="About">About</Nav.Link>
-                            <Nav.Link href="Contact">Contact</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
+        <BrowserRouter>
+            <div className="NavbarComp">
+                <>
+                    <Navbar bg="dark" variant="dark" expand="lg">
+                        <Container>
+                            <Navbar.Brand>
+                                Logo{" "}
+                                <img src={logo} width="40px" height="40px" />
+                            </Navbar.Brand>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="basic-navbar-nav">
+                                <Nav className="me-auto">
+                                    <Nav.Link as={Link} to="/">
+                                        Home
+                                    </Nav.Link>
+                                    <Nav.Link as={Link} to="About">
+                                        About
+                                    </Nav.Link>
+                                    <Nav.Link as={Link} to="Contact">
+                                        Contact
+                                    </Nav.Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
+                </>
+            </div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
